ac8ab9a581ac61406dd5b125a9f717d8aee442ab
Rakesh Singh <rakeshsingh@Rakeshs-MBP.lan>
2022-09-21 10:33:55 -0700

    add project component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,8 +4,8 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import editor from "../../Assets/Projects/BulletinProjectFrame.png";
+import chatify from "../../Assets/Projects/MoviePediaProjectFrame.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
